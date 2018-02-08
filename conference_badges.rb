c7d4438153c5612d0d500c1b7848c40603533054
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-08 00:25:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 def badge_maker(name)
-  "Hello my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(names)
   output = []
   for i in names do
-    output << "Hello my name is #{names[i]}."
+    output << "Hello, my name is #{names[i]}."
   end
   output
 end
