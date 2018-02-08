f0920964d56943343d0500492c0d2a0dfb7beccd
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-08 00:21:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   "Hello my name is #{name}."
 end
+
+def batch_badge_creator(names)
+  output = []
+  for i in names do
+    output << "Hello my name is #{names[i]}."
+  end
+  output
+end
